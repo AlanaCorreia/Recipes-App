7d732428a09ba1b37fdeea2264f5ce0a92cc66a7
Alana Correia <alanacorreia.ac@gmail.com>
2022-03-08 19:03:00 -0300

    requisitos 82-84

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,9 +2,32 @@ import React from 'react';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const userEmail = JSON.parse(localStorage.getItem('user')).email;
+  console.log(userEmail);
   return (
     <div>
-      <h1>Profile</h1>
+      <p data-testid="profile-email">{userEmail}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        // onClick={ handleClick }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        // onClick={ handleClick }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        // onClick={ handleClick }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
