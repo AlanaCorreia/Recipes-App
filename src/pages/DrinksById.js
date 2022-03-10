77dead45068795dcaf63cbdd90a9ec679b4fa7d0
Alana Correia <alanacorreia.ac@gmail.com>
2022-03-10 17:25:28 -0300

    requisitos 33 e 34

--- src/pages/DrinksById.js
+++ src/pages/DrinksById.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
 function DrinksById() {
+  const { location: { pathname } } = useHistory();
+  const [recipeDrink, setRecipeDrink] = useState([]);
+
+  const id = pathname.replace(/[^0-9]/g, '');
+
+  async function getFetchDrinkApi() {
+    const resultsApi = await fetchDrinkApi(`lookup.php?i=${id}`);
+    setRecipeDrink(resultsApi.meals);
+  }
+
+  useEffect(() => {
+    getFetchDrinkApi();
+  }, []);
+
+  console.log(recipeDrink);
+
   return (
     <div>
-      <h1>Drinks Details</h1>
+      <img data-testid="recipe-photo" src="" alt="imgTest" />
+      <h1 data-testid="recipe-title">Title Details Food</h1>
+      <button data-testid="share-btn" type="button">
+        share button
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        favorite button
+      </button>
+      <p data-testid="recipe-category">category</p>
+      <ul>
+        <li data-testid={ `${0}-ingredient-name-and-measure` }>Ingredient 1</li>
+      </ul>
+      <p data-testid="instructions">instructions</p>
+      <div data-testid={ `${0}-recomendation-card` }>
+        <p>recomendação</p>
+      </div>
+      <button data-testid="start-recipe-btn" type="button">
+        Start recipe
+      </button>
     </div>
   );
 }
--- src/pages/FoodsById.js
+++ src/pages/FoodsById.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function FoodsById() {
+  const { location: { pathname } } = useHistory();
+  const [recipeFood, setRecipeFood] = useState([]);
+
+  const id = pathname.replace(/[^0-9]/g, '');
+
+  async function getFetchFoodApi() {
+    const resultsApi = await fetchFoodApi(`lookup.php?i=${id}`);
+    setRecipeFood(resultsApi.meals);
+  }
+
+  useEffect(() => {
+    getFetchFoodApi();
+  }, []);
+
+  console.log(recipeFood);
+
   return (
     <div>
       <img data-testid="recipe-photo" src="" alt="imgTest" />
@@ -13,11 +30,11 @@ function FoodsById() {
       </button>
       <p data-testid="recipe-category">category</p>
       <ul>
-        <li data-testid={`${0}-ingredient-name-and-measure`}>Ingredient 1</li>
+        <li data-testid={ `${0}-ingredient-name-and-measure` }>Ingredient 1</li>
       </ul>
       <p data-testid="instructions">instructions</p>
-      <iframe data-testid="video" />
-      <div data-testid={`${0}-recomendation-card`}>
+      <iframe data-testid="video" title="video" />
+      <div data-testid={ `${0}-recomendation-card` }>
         <p>recomendação</p>
       </div>
       <button data-testid="start-recipe-btn" type="button">
