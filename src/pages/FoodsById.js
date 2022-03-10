f187ccf6fb73937afe55712217aee6b42a8807a2
Alana Correia <alanacorreia.ac@gmail.com>
2022-03-10 16:39:57 -0300

    requisito 33

--- src/pages/FoodsById.js
+++ src/pages/FoodsById.js
@@ -3,7 +3,26 @@ import React from 'react';
 function FoodsById() {
   return (
     <div>
-      <h1>Foods Details</h1>
+      <img data-testid="recipe-photo" src="" alt="imgTest" />
+      <h1 data-testid="recipe-title">Title Details Food</h1>
+      <button data-testid="share-btn" type="button">
+        share button
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        favorite button
+      </button>
+      <p data-testid="recipe-category">category</p>
+      <ul>
+        <li data-testid={`${0}-ingredient-name-and-measure`}>Ingredient 1</li>
+      </ul>
+      <p data-testid="instructions">instructions</p>
+      <iframe data-testid="video" />
+      <div data-testid={`${0}-recomendation-card`}>
+        <p>recomendação</p>
+      </div>
+      <button data-testid="start-recipe-btn" type="button">
+        Start recipe
+      </button>
     </div>
   );
 }
