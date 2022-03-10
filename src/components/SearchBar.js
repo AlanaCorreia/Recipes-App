e124aa53ae7eccce89b6ecf422ae18fb9bd204f7
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-03-10 02:13:53 -0300

    Tela de SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext /* useEffect */ } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import MyContext from '../context/myContext';
@@ -7,7 +7,7 @@ import fetchFoodApi from '../services/fetchApiFood';
 
 function SearchBar({ name }) {
   const [radioValue, setRadioValue] = useState('');
-  const [apiResults, setApiResults] = useState({ [name]: [] });
+  // const [apiResults, setApiResults] = useState({ [name]: [] });
   const [apiResultsSplited, setApiResultsSplited] = useState({ [name]: [] });
 
   const { searchInput } = useContext(MyContext);
@@ -15,6 +15,7 @@ function SearchBar({ name }) {
   const history = useHistory();
   const DOZE = 12;
 
+  /*
   useEffect(() => {
     if (apiResults[name] !== undefined) {
       if (apiResults[name].length >= DOZE) {
@@ -31,6 +32,7 @@ function SearchBar({ name }) {
       }
     }
   }, [apiResults]);
+  */
 
   function handleRadio({ target }) {
     setRadioValue(target.value);
@@ -39,46 +41,118 @@ function SearchBar({ name }) {
   async function searchFoods() {
     if (radioValue === 'ingrediente') {
       const result = await fetchFoodApi(`filter.php?i=${searchInput}`);
-      setApiResults(result);
+      // setApiResults(result);
+      return result;
     } if (radioValue === 'nome') {
       const result = await fetchFoodApi(`search.php?s=${searchInput}`);
-      setApiResults(result);
+      // setApiResults(result);
+      return result;
     } if (radioValue === 'letra') {
       if (searchInput.length <= 1) {
         const result = await fetchFoodApi(`search.php?f=${searchInput}`);
-        setApiResults(result);
-      } else {
-        global.alert('Your search must have only 1 (one) character');
+        // setApiResults(result);
+        return result;
       }
+      global.alert('Your search must have only 1 (one) character');
     }
   }
 
   async function searchDrinks() {
     if (radioValue === 'ingrediente') {
       const result = await fetchDrinkApi(`filter.php?i=${searchInput}`);
-      setApiResults(result);
+      // setApiResults(result);
+      return result;
     } if (radioValue === 'nome') {
       const result = await fetchDrinkApi(`search.php?s=${searchInput}`);
-      setApiResults(result);
+      // setApiResults(result);
+      return result;
     } if (radioValue === 'letra') {
       if (searchInput.length <= 1) {
         const result = await fetchDrinkApi(`search.php?f=${searchInput}`);
-        setApiResults(result);
+        // setApiResults(result);
+        return result;
+      }
+      global.alert('Your search must have only 1 (one) character');
+    }
+  }
+
+  async function mealsCondition() {
+    const returnFoodsApi = await searchFoods();
+    if (returnFoodsApi !== undefined) {
+      if (returnFoodsApi.meals === null) {
+        global.alert('Sorry, we haven\'t found any recipes for these filters.');
+      } else if (returnFoodsApi.meals.length === 1) {
+        history.push(`/foods/${returnFoodsApi.meals[0].idMeal}`);
+      } else if (returnFoodsApi.meals.length >= DOZE) {
+        const splitedArray = returnFoodsApi[name].slice(0, DOZE);
+        setApiResultsSplited({ [name]: splitedArray });
       } else {
-        global.alert('Your search must have only 1 (one) character');
+        setApiResultsSplited({ [name]: returnFoodsApi.meals });
       }
     }
   }
 
-  async function fetchApi() {
-    if (name === 'meals') {
-      await searchFoods();
-    } else {
-      await searchDrinks();
+  async function drinkCondition() {
+    const returnDrinkApi = await searchDrinks();
+    if (returnDrinkApi !== undefined) {
+      if (returnDrinkApi.drinks === null) {
+        global.alert('Sorry, we haven\'t found any recipes for these filters.');
+      } else if (returnDrinkApi.drinks.length === 1) {
+        history.push(`/drinks/${returnDrinkApi.drinks[0].idDrink}`);
+      } else if (returnDrinkApi.drinks.length >= DOZE) {
+        const splitedArray = returnDrinkApi[name].slice(0, DOZE);
+        setApiResultsSplited({ [name]: splitedArray });
+      } else {
+        setApiResultsSplited({ [name]: returnDrinkApi.meals });
+      }
     }
-    console.log(apiResults);
   }
 
+  function fetchApi() {
+    if (name === 'meals') {
+      mealsCondition();
+    } else {
+      drinkCondition();
+    }
+  }
+
+  function renderCards() {
+    return apiResultsSplited[name].length > 0 && name === 'meals' ? (
+      apiResultsSplited[name].map(({ strMeal, strMealThumb }, index) => (
+        <div
+          key={ strMeal }
+          data-testid={ `${index}-recipe-card` }
+        >
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ strMealThumb }
+            alt={ strMeal }
+          />
+          <p data-testid={ `${index}-card-name` }>
+            {' '}
+            { strMeal }
+          </p>
+        </div>
+      ))
+    ) : (
+      apiResultsSplited[name].map(({ strDrink, strDrinkThumb }, index) => (
+        <div
+          key={ strDrink }
+          data-testid={ `${index}-recipe-card` }
+        >
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ strDrinkThumb }
+            alt={ strDrink }
+          />
+          <p data-testid={ `${index}-card-name` }>
+            {' '}
+            { strDrink }
+          </p>
+        </div>
+      ))
+    );
+  }
   return (
     <div>
       <label htmlFor="ingrediente">
@@ -123,29 +197,8 @@ function SearchBar({ name }) {
         onClick={ fetchApi }
       >
         Search
-
       </button>
-      {console.log(apiResultsSplited)}
-      {
-        apiResultsSplited[name].length > 0 && (
-          apiResultsSplited[name].map(({ strMeal, strMealThumb }, index) => (
-            <div
-              key={ strMeal }
-              data-testid={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ strMealThumb }
-                alt={ strMeal }
-              />
-              <p data-testid={ `${index}-card-name` }>
-                {' '}
-                { strMeal }
-              </p>
-            </div>
-          ))
-        )
-      }
+      {renderCards()}
     </div>
   );
 }
