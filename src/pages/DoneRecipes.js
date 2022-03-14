322ccb44b5218e7fafe28d6f275f766e0e6bf525
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-03-14 16:04:20 -0300

    Requisito 55 e 56

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -27,6 +27,13 @@ const doneRecipes = [
   },
 ];
 
+function printTags(arrayTags) {
+  if (arrayTags.length > 2) {
+    return arrayTags.slice(0, 2);
+  }
+  return arrayTags;
+}
+
 function DoneRecipes() {
   return (
     <div>
@@ -34,46 +41,83 @@ function DoneRecipes() {
         <Header name="Done Recipes" />
       </div>
       <Header2 />
-      {doneRecipes.map((element, index) => (
-        <div key={ element.id }>
-          <img
-            src={ element.image }
-            alt={ element.name }
-            data-testid={ `${index}-horizontal-image` }
-            style={ { width: '200px' } }
-          />
-          <p
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {element.category}
-          </p>
-          <p
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {element.name}
-          </p>
-          <p
-            data-testid={ `${index}-horizontal-done-date` }
-          >
-            {element.doneDate}
-          </p>
-          <button
-            type="button"
-            data-testid={ `${index}-horizontal-share-btn` }
-          >
-            Share
-          </button>
-          <ul>
-            {element.tags && element.tags.map((tagElement) => (
-              <li
-                key={ element.id }
-                data-testid={ `${index}-${tagElement}-horizontal-tag` }
+      {doneRecipes.map((element, index) => {
+        if (element.type === 'food') {
+          return (
+            <div key={ element.id }>
+              <img
+                src={ element.image }
+                alt={ element.name }
+                data-testid={ `${index}-horizontal-image` }
+                style={ { width: '200px' } }
+              />
+              <p
+                data-testid={ `${index}-horizontal-name` }
               >
-                {tagElement}
-              </li>))}
-          </ul>
-        </div>
-      ))}
+                {element.name}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${element.nationality} - ${element.category}`}
+              </p>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {element.doneDate}
+              </p>
+              <ul>
+                {element.tags.length >= 1 && printTags(element.tags).map((tagElement) => (
+                  <li
+                    key={ element.id }
+                    data-testid={ `${index}-${tagElement}-horizontal-tag` }
+                  >
+                    {tagElement}
+                  </li>))}
+              </ul>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src="src/images/shareIcon.svg"
+              >
+                Share
+              </button>
+            </div>
+          );
+        }
+        return (
+          <div key={ element.id }>
+            <img
+              src={ element.image }
+              alt={ element.name }
+              data-testid={ `${index}-horizontal-image` }
+              style={ { width: '200px' } }
+            />
+            <p
+              data-testid={ `${index}-horizontal-name` }
+            >
+              {element.name}
+            </p>
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {element.alcoholicOrNot}
+            </p>
+            <p
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              {element.doneDate}
+            </p>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              src="src/images/shareIcon.svg"
+            >
+              Share
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
