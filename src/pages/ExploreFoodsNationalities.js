06baccc008421fbfcdbbcd409997db3b86e09260
Alana Correia <alanacorreia.ac@gmail.com>
2022-03-16 16:40:59 -0300

    requsito 80

--- src/pages/ExploreFoodsNationalities.js
+++ src/pages/ExploreFoodsNationalities.js
@@ -3,15 +3,17 @@ import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchIcon from '../components/SearchIcon';
-import { MAX_NUMBER_CARDS } from '../services/consts';
+import { DEFAULT_URL_API, MAX_NUMBER_CARDS } from '../services/consts';
 import fetchApiArea from '../services/fetchApiArea';
 import fecthNacionalitesApi from '../services/fetchApiNacionalites';
 import '../ExploreNationalities.css';
+import fetchFoodApi from '../services/fetchApiFood';
 
 function ExploreFoodsNationalities() {
-  const [nacionalities, setNacionalities] = useState([]);
-  const [selectValue, setSelectValue] = useState('American');
+  const [nacionalities, setNacionalities] = useState(['All']);
+  const [selectValue, setSelectValue] = useState('All');
   const [mealsOfArea, setMealsOfArea] = useState([]);
+  // const [allFoods, setAllFoods] = useState([]);
 
   const history = useHistory();
 
@@ -20,20 +22,28 @@ function ExploreFoodsNationalities() {
     const dataNacionalities = await fecthNacionalitesApi();
     const nacionalitiesKeys = dataNacionalities
       .map(({ strArea }) => (strArea));
-    setNacionalities(nacionalitiesKeys);
+    setNacionalities([...nacionalities, ...nacionalitiesKeys]);
+  };
+
+  const defaultAPI = async () => {
+    const foodDefault = await fetchFoodApi(DEFAULT_URL_API);
+    const sliceFoodDefault = foodDefault.meals.slice(0, MAX_NUMBER_CARDS);
+    setMealsOfArea(sliceFoodDefault);
   };
 
   // Faz requisição das receitas baseadas na nacionalidade.
   // Pega as 12 primeiras receitas e armazena no estado mealsOfArea
-  const getFetchArea = useCallback(async () => {
+  const getFetchArea = async () => {
     const areaFoods = await fetchApiArea(selectValue);
+    if (areaFoods === null) {
+      return;
+    }
     setMealsOfArea(areaFoods.slice(0, MAX_NUMBER_CARDS));
-  }, [selectValue]);
+  };
 
   // Lida com valores da Api armazenados no estado mealsOfArea
   // Nos casos não encontrar receita ou se houver apenas 1 receita
   const mealsCondition = useCallback(async () => {
-    console.log('checa condition');
     if (mealsOfArea !== undefined) {
       if (mealsOfArea === null) {
         global.alert('Sorry, we haven\'t found any recipes for these filters.');
@@ -47,21 +57,24 @@ function ExploreFoodsNationalities() {
   useEffect(() => { getFetchNacionalities(); }, []);
 
   // Atualiza quando o componente é montado e quando a função é atualizada em razão do selectValue
-  useEffect(() => { getFetchArea(); }, [getFetchArea]);
+  // useEffect(() => { getFetchArea(); }, [getFetchArea]);
+
+  useEffect(() => {
+    if (selectValue === 'All') {
+      defaultAPI();
+    } getFetchArea();
+  }, [selectValue]);
 
   // Atualiza quando o componente é montado e quando a função é atualizada em razão do mealsOfArea
   useEffect(() => { mealsCondition(); }, [mealsCondition]);
 
   // Seleção da option seta País no estado selectValue
   function handleChange({ target }) {
-    console.log('clicou');
     setSelectValue(target.value);
   }
 
   // Renderiza os cards de receita
   function renderCards() {
-    console.log('renderizou');
-    console.log(mealsOfArea);
     if (mealsOfArea.length > 0) {
       return mealsOfArea.map(({ strMeal, strMealThumb }, index) => (
         <div
